3e2cb76baa7f45350d705a84fcd1b5185d62dbdb
c.wohlbrecht <c.wohlbrecht@finatix.de>
2026-09-22 16:33:16 +0200

    rename interfaces

---
+++ src/main/java/de/chriswohlbrecht/maintenance/component/IMaintenanceLogComponent.java
@@ -0,0 +1,20 @@
+package de.chriswohlbrecht.maintenance.component;
+
+import de.chriswohlbrecht.maintenance.api.model.MaintenanceLogRequest;
+import de.chriswohlbrecht.maintenance.api.model.MaintenanceLogResponse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IMaintenanceLogComponent {
+
+    Optional<List<MaintenanceLogResponse>> listMaintenanceLogs(Long vehicleId);
+
+    Optional<MaintenanceLogResponse> getMaintenanceLog(Long vehicleId, Long logId);
+
+    Optional<MaintenanceLogResponse> createMaintenanceLog(Long vehicleId, MaintenanceLogRequest request);
+
+    Optional<MaintenanceLogResponse> updateMaintenanceLog(Long vehicleId, Long logId, MaintenanceLogRequest request);
+
+    boolean deleteMaintenanceLog(Long vehicleId, Long logId);
+}
